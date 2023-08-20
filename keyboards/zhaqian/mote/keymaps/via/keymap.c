514d730ad4b59d47854de8c5c999de2a9ab0917e
kpaxart <07genics@gmail.com>
2023-08-21 01:04:28 +0800

    整理keymap

--- keyboards/zhaqian/mote/keymaps/via/keymap.c
+++ keyboards/zhaqian/mote/keymaps/via/keymap.c
@@ -7,14 +7,15 @@ enum layer_number {
     // clang-format off
     _QWERTY = 0,
     _GAME = 1,
-    _NAV= 2,
-    _NUMBER= 3,
-    _SYMBOL= 4,
-    _FUNC= 5,
-    _SYS
+    _NAV = 2,
+    _NUMBER = 3,
+    _SYMBOL = 4,
+    _FUNC = 5,
+    _SYS = 6
     // clang-format on
 };
 
+#define XXXX KC_TRNS
 // Left-hand home row mods
 #define HOME_A LGUI_T(KC_A)
 #define HOME_S LALT_T(KC_S)
@@ -74,32 +75,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_GAME] = LAYOUT(
   // ╭───────────────────────────────────────────────────╮            ╭───────────────────────────────────────────────────╮
-      KC_Q,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                 KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_5,
+      XXXX,  XXXX,  XXXX,   XXXX,   XXXX,   XXXX,                       XXXX,   XXXX,   XXXX,   XXXX,   XXXX,   XXXX,
   // ├───────────────────────────────────────────────────┤            ├───────────────────────────────────────────────────┤
-      KC_Q,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_P,
+      XXXX,  XXXX,  XXXX,   XXXX,   XXXX,   XXXX,                       XXXX,   XXXX,   XXXX,   XXXX,   XXXX,   XXXX,
   // ├───────────────────────────────────────────────────┤            ├───────────────────────────────────────────────────┤
-      KC_A,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                 KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_P,
+      XXXX,  XXXX,  XXXX,   XXXX,   XXXX,   XXXX,                       XXXX,   XXXX,   XXXX,   XXXX,   XXXX,   XXXX,
   // ├───────────────────────────────────────────────────┤            ├───────────────────────────────────────────────────┤
-      KC_Z,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                 KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_P,
+      XXXX,  XXXX,  XXXX,   XXXX,   XXXX,   XXXX,                       XXXX,   XXXX,   XXXX,   XXXX,   XXXX,   XXXX,
   // ╰───────────────────────────────────────────────────┤            ├───────────────────────────────────────────────────╯
-              KC_A,    KC_A,    KC_A,     MO(2), CW_TOGG,               KC_SPC,  MO(1),   KC_A,    KC_A,   KC_A,
+             XXXX,  XXXX,   XXXX,   XXXX,   XXXX,                       XXXX,   XXXX,   XXXX,   XXXX,   XXXX,
   //       ╰─────────────────────────────────────────────╯            ╰──────────────────────────────────────────╯
-                           KC_UP, KC_DOWN, KC_LEFT,KC_RIGHT,              KC_UP, KC_DOWN, KC_LEFT,KC_RIGHT
+                     XXXX,  XXXX,   XXXX,   XXXX,                       XXXX,  XXXX,   XXXX,   XXXX
     ),
 
     [_NAV] = LAYOUT(
   // ╭───────────────────────────────────────────────────╮            ╭───────────────────────────────────────────────────╮
-      KC_Q,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                 KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_5,
+      XXXX,  XXXX,  XXXX,   XXXX,   XXXX,   XXXX,                       XXXX,   XXXX,   XXXX,   XXXX,   XXXX,   XXXX,
   // ├───────────────────────────────────────────────────┤            ├───────────────────────────────────────────────────┤
-      KC_Q,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_P,
+      XXXX,  XXXX,  XXXX,   XXXX,   XXXX,   XXXX,                       XXXX,   XXXX,   XXXX,   XXXX,   XXXX,   XXXX,
   // ├───────────────────────────────────────────────────┤            ├───────────────────────────────────────────────────┤
-      KC_A,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                 KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_P,
+      XXXX,  XXXX,  XXXX,   XXXX,   XXXX,   XXXX,                       XXXX,   XXXX,   XXXX,   XXXX,   XXXX,   XXXX,
   // ├───────────────────────────────────────────────────┤            ├───────────────────────────────────────────────────┤
-      KC_Z,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_P,
+      XXXX,  XXXX,  XXXX,   XXXX,   XXXX,   XXXX,                       XXXX,   XXXX,   XXXX,   XXXX,   XXXX,   XXXX,
   // ╰───────────────────────────────────────────────────┤            ├───────────────────────────────────────────────────╯
-              KC_A,    KC_A,    KC_A,     MO(2), CW_TOGG,               KC_SPC,  MO(1),   KC_A,    KC_A,   KC_A,
+             XXXX,  XXXX,   XXXX,   XXXX,   XXXX,                       XXXX,   XXXX,   XXXX,   XXXX,   XXXX,
   //       ╰─────────────────────────────────────────────╯            ╰──────────────────────────────────────────╯
-                           KC_UP, KC_DOWN, KC_LEFT,KC_RIGHT,              KC_UP, KC_DOWN, KC_LEFT,KC_RIGHT
+                     XXXX,  XXXX,   XXXX,   XXXX,                       XXXX,  XXXX,   XXXX,   XXXX
     ),
 
     [_NUMBER] = LAYOUT(
